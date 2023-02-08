becd8764d8587adf2335727a7f01da2c269f84a3
pratikshabathe <pratikshabathe11@gmail.com>
2023-02-08 19:14:01 +0530

    Added: Enter  Valid Last Name

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -8,13 +8,19 @@ public class UserRegistrationProblem {
 
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("--Welcom to user resistration problems--");
+		System.out.println("--Welcom to user resistration problem--");
 		checkFirstName();
+		checkLastName();
 	}
 		public static void checkFirstName() {
-			System.out.println("Enter first number: ");
+			System.out.println("Enter first name: ");
 			String fName = sc.next();
 			userRegistration(fName);
+			}
+		public static void checkLastName() {
+			System.out.println("Enter Last name: ");
+			String lName = sc.next();
+			userRegistration(lName);
 			}
 		
 		public static void userRegistration(String firstName) {
