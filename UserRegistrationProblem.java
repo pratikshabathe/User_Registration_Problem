7b72db6f4f94c8a72fa32227503c51b8c470655e
pratikshabathe <pratikshabathe11@gmail.com>
2023-02-08 19:00:15 +0530

    Added: Enter valid first name

---
+++ UserRegistrationProblem.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.user_registration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("--Welcom to user resistration problems--");
+		checkFirstName();
+	}
+		public static void checkFirstName() {
+			System.out.println("Enter first number: ");
+			String fName = sc.next();
+			userRegistration(fName);
+			}
+		
+		public static void userRegistration(String firstName) {
+			String regex = "^[A-Z]{1}[a-z]{2,}$";
+			Pattern p = Pattern.compile(regex);
+			Matcher m = p.matcher(firstName);
+			if(m.matches())
+				System.out.println("valid");
+			else
+				System.out.println("Invalid");
+			
+		}
+}
